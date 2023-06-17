664bcee154eb560c18477f1363d6100628ace598
Lucas Deray <lucas.deray@greenscope.io>
2023-06-17 13:59:23 +0200

    show error message on signup error

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { redirect } from 'react-router-dom'
 
 const Signup: React.FC = () => {
+    const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [error, setError] = useState('')
@@ -13,15 +14,20 @@ const Signup: React.FC = () => {
             const response = await axios.post(
                 `http://localhost:${process.env.REACT_APP_SERVER_PORT}/api/auth/signup`,
                 {
+                    user_name: username,
                     email,
                     password,
-                }
+                },
+                { withCredentials: true }
             )
             const { token } = response.data
             // Store the token in localStorage or a state management solution
             // Redirect the user or update the UI based on the successful signup
         } catch (error) {
-            setError('Error occurred during signup')
+            setError(
+                `Error occurred during signup: ${error.response.data.error}`
+            )
+            console.error(error)
         }
     }
 
@@ -30,6 +36,15 @@ const Signup: React.FC = () => {
             <h2>Signup</h2>
             {error && <p>{error}</p>}
             <form onSubmit={handleSignup}>
+                <div>
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
                 <div>
                     <label>Email:</label>
                     <input
